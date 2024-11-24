bddb9d718f2c508d02d73761f4cae3c659f86250
陈欣培 <chenxinpei@chenxinpeideMacBook-Pro.local>
2024-11-24 14:55:19 +0800

    run openai-translator

--- ai_translator/main.py
+++ ai_translator/main.py
@@ -13,14 +13,21 @@ if __name__ == "__main__":
     config_loader = ConfigLoader(args.config)
 
     config = config_loader.load_config()
+    LOG.info(f"Config: {config}")
 
-    model_name = args.openai_model if args.openai_model else config['OpenAIModel']['model']
-    api_key = args.openai_api_key if args.openai_api_key else config['OpenAIModel']['api_key']
+    model_name = config['OpenAIModel']['model']
+    api_key = config['OpenAIModel']['api_key']
+    base_url = config['OpenAIModel']['base_url']
+    if args.model_type == 'OpenAIModel' and (not model_name or not api_key):
+        LOG.error("--openai_model and --openai_api_key is required when using OpenAIModel")
+    
+    os.environ["OPENAI_API_KEY"] = api_key
+    os.environ["OPENAI_BASE_URL"] = base_url
+    
     model = OpenAIModel(model=model_name, api_key=api_key)
 
-
-    pdf_file_path = args.book if args.book else config['common']['book']
-    file_format = args.file_format if args.file_format else config['common']['file_format']
+    pdf_file_path = config['common']['book']
+    file_format = config['common']['file_format']
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
